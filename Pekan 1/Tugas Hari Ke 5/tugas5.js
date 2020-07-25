92947e1d30ba69e614d7c149dc77dc509824cfff
Seruni Dewanti <68325578+serunisheshe@users.noreply.github.com>
2020-07-25 11:16:29 +0700

    Membuat Tugas 5, Materi Function

---
+++ Pekan 1/Tugas Hari Ke 5/tugas5.js
@@ -0,0 +1,30 @@
+//Soal 1
+function halo() {
+    return "Halo Sanbers!";
+}
+
+console.log(halo());
+
+//Soal 2
+function kalikan(num1, num2) {
+    return num1 * num2;
+}
+
+var num1 = 12
+var num2 = 4
+
+var hasilKali = kalikan(num1, num2)
+console.log(hasilKali)
+
+//Soal 3
+function introduce(name, age, address, hobby) {
+    return "Nama Saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ". dan saya punya hobby yaitu " + hobby + "!";
+}
+
+var name = "John"
+var age = 30
+var address = "Jalan belum jadi"
+var hobby = "Gaming"
+
+var perkenalan = introduce(name, age, address, hobby)
+console.log(perkenalan)
